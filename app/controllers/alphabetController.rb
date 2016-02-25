f620d6297c2c99e9f04eefef1eb46cffc4c441f2
Sam Matthews <sammatthewsnz@gmail.com>
2016-02-25 22:51:15 +1300

    Import of current project (very basic) of RubyMotion.

---
+++ app/controllers/alphabetController.rb
@@ -0,0 +1,48 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+
+    new_frame = self.view.frame
+    new_frame.size.height -= self.tabBarController.tabBar.bounds.size.height
+    self.view.frame = new_frame
+
+    self.title = "Count Me Up"
+
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    self.view.addSubview @table
+
+    @table.dataSource = self
+    @table.delegate = self
+
+    @data = ['','Harry','Sophia','Sam']
+    
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+
+    # put your data in the cell
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the number of rows
+    @data.count
+  end
+
+  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    # When user selects a row
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+    alert = UIAlertView.alloc.init
+    alert.message = "You tapped #{@data[indexPath.row]} Sam!"
+    alert.addButtonWithTitle "Oh No"
+    alert.show
+  end
+end
